cc0ea19ec5f98f2356b4726cd8a2ffa7e5c1d06a
luckasmatos <luckas.matos@hotmail.com>
2023-12-17 13:06:26 -0300

    first commit

--- src/config/multer.js
+++ src/config/multer.js
@@ -4,7 +4,7 @@ import { extname, resolve, dirname } from 'path'
 
 const multerConfig = { 
   storage: multer.diskStorage({ 
-    destination: resolve(__dirname, '..', '..', 'uploads'),
+    destination: resolve(__dirname, "..", "..", "uploads"),
     filename: (request, file, callback) => {
       return callback(null, uuidv4() + extname(file.originalname))
     }
