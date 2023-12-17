b4e78632b4aefa8b8c4565d257c0fd101a3cda99
luckasmatos <luckas.matos@hotmail.com>
2023-12-17 10:49:28 -0300

    first commit

--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -8,7 +8,7 @@ class Category extends Model {
             url: {
                 type: Sequelize.VIRTUAL,
                 get(){
-                    return `http://localhost:3001/category-file/${this.path}`
+                    return `https://codeburger-api-production.up.railway.app/category-file/${this.path}`
                 },
             },
         },{
