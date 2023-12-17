0179ab8eaba9cda0b7217b8f2076c40a8510ec38
luckasmatos <luckas.matos@hotmail.com>
2023-12-17 10:42:00 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -2,11 +2,18 @@ import express from "express"
 import routes from "./routes.js"
 import './database/index.js'
 import { resolve } from 'path'
+import cors from 'cors'
+
+const corsOptions = {
+  origin: 'https://luckas-codeburger.vercel.app',
+  credentials: true,
+}
 
 class App {
   constructor() {
     this.app = express()
     this.middlewares()
+    this.app.use(cors(corsOptions))
     this.routes()
   }
 
